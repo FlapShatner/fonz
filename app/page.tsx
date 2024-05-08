2dc4cb040af0ade214d8569337fa4b5c53d1a045
Jordan Roberts <jrobertswebdev@gmail.com>
2024-05-07 20:02:20 -0500

    connect to storefront api

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
 'use client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { Shop } from '@shopify/hydrogen-react/storefront-api-types'
 
 export default function Home() {
- const [products, setProducts] = useState([])
+ const [shop, setShop] = useState<Shop | null>(null)
+
+ useEffect(() => {
+  const fetchShop = async () => {
+   const shop = await fetch('/api/shop')
+   const json = await shop.json()
+   console.log(json)
+   setShop(json.data.shop)
+  }
+  fetchShop()
+ }, [])
+
  return (
   <main className='flex min-h-screen flex-col items-center justify-between p-24'>
    <h1 className='text-4xl font-bold'>HEYO!</h1>
-   <div className='p-2 border border-border'>get 10 Products</div>
+   <div className='p-2 border border-border'>{shop && shop.name}</div>
   </main>
  )
 }
