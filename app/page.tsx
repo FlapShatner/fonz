ceef4fd52a391168a47534fcb3d48f3e40ea45e6
Jordan Roberts <jrobertswebdev@gmail.com>
2024-05-08 10:03:59 -0500

    pull origin

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 'use client'
 import { useState } from 'react'
+import { client } from './lib/shopify-client'
 
 export default function Home() {
  const [products, setProducts] = useState([])
+ const getProducts = async () => {}
  return (
   <main className='flex min-h-screen flex-col items-center justify-between p-24'>
    <h1 className='text-4xl font-bold'>HEYO!</h1>
-   <div className='p-2 border border-border'>get 10 Products</div>
+   <div
+    onClick={getProducts}
+    className='p-2 border border-border cursor-pointer'>
+    get 10 Products
+   </div>
   </main>
  )
 }
