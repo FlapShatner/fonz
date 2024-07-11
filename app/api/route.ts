4912cfcb9796cac1768e5db70bb099ec83e989ad
Jordan Roberts <jrobertswebdev@gmail.com>
2024-07-11 13:29:43 -0500

    delete api directory

--- app/api/route.ts
+++
@@ -1,7 +0,0 @@
-// import { nlpTest } from '../lib/compromise'
-// export const dynamic = 'force-dynamic' // defaults to auto
-
-// export async function GET(request: Request) {
-//  const result = await nlpTest()
-//  return new Response(JSON.stringify(result), { status: 200 })
-// }
